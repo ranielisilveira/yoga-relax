a60669881b18f7caddff2cc9d01699ea82ffe4d3
Ranieli <ranielisilveira@hotmail.com>
2021-02-10 12:13:49 -0300

    feat: store multiple videos

--- app/Http/Controllers/Admin/MediaController.php
+++ app/Http/Controllers/Admin/MediaController.php
@@ -1,222 +1,247 @@
 +<?php
 
-namespace App\Http\Controllers\Admin;
+    namespace App\Http\Controllers\Admin;
 
-use App\Enum\EnumMediaTypes;
-use App\Helpers\ValidateJsonLangKey;
-use App\Http\Controllers\Controller;
-use App\Models\Media;
-use Exception;
-use Illuminate\Validation\Rule;
-use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
+    use App\Enum\EnumMediaTypes;
+    use App\Helpers\ValidateJsonLangKey;
+    use App\Http\Controllers\Controller;
+    use App\Models\Media;
+    use Exception;
+    use Illuminate\Validation\Rule;
+    use Illuminate\Http\Request;
+    use Illuminate\Support\Facades\DB;
+    use Symfony\Component\HttpFoundation\Response;
 
-class MediaController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index(Request $request)
+    class MediaController extends Controller
     {
-        $medias = Media::withTrashed()
-            ->with([
-                'category' => function ($category) {
-                    $category->withTrashed()->with([
-                        'category' => function ($middleCategory) {
-                            $middleCategory->withTrashed()->with([
-                                'category' => function ($parentCategory) {
-                                    $parentCategory->withTrashed();
-                                }
-                            ]);
-                        }
-                    ]);
-                }
+        /**
+         * Display a listing of the resource.
+         *
+         * @return \Illuminate\Http\Response
+         */
+        public function index(Request $request)
+        {
+            $medias = Media::withTrashed()
+                ->with([
+                    'category' => function ($category) {
+                        $category->withTrashed()->with([
+                            'category' => function ($middleCategory) {
+                                $middleCategory->withTrashed()->with([
+                                    'category' => function ($parentCategory) {
+                                        $parentCategory->withTrashed();
+                                    }
+                                ]);
+                            }
+                        ]);
+                    }
                 ])
                 ->whereHas('category')
                 ->orderBy('deleted_at');
 
-        if ($request->search) {
-            $medias->where('media', 'LIKE', "%{$request->search}%");
-        }
-
-        return $medias->paginate((int)($request->length ?? 10));
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $this->validate($request, [
-            'category_id' => 'nullable|exists:categories,id',
-            'type' => [
-                'required',
-                Rule::in(array_keys(EnumMediaTypes::TYPES_ARRAY))
-            ]
-        ], []);
-
-        if ($request->type == EnumMediaTypes::PDF) {
-            $this->validate($request, ['media' => 'required|file|mimes:pdf,doc,jpeg,png']);
-        }
-
-        if ($request->type == EnumMediaTypes::VIDEO || $request->type == EnumMediaTypes::TEXT) {
-            $this->validate($request, ['media' => 'required|json']);
-        }
-
-        try {
-            ValidateJsonLangKey::validate($request->media);
-
-            if ($request->type == EnumMediaTypes::VIDEO) {
-                $request['media'] = str_replace([
-                    "https://vimeo.com/",
-                    "http://vimeo.com/",
-                    "https://player.vimeo.com/",
-                    "http://player.vimeo.com/",
-                ], '', $request->media);
+            if ($request->search) {
+                $medias->where('media', 'LIKE', "%{$request->search}%");
             }
 
-            //TODO: file upload
+            return $medias->paginate((int)($request->length ?? 10));
+        }
+
+        /**
+         * Store a newly created resource in storage.
+         *
+         * @param  \Illuminate\Http\Request  $request
+         * @return \Illuminate\Http\Response
+         */
+        public function store(Request $request)
+        {
+            $this->validate($request, [
+                'category_id' => 'nullable|exists:categories,id',
+                'type' => [
+                    'required',
+                    Rule::in(array_keys(EnumMediaTypes::TYPES_ARRAY))
+                ]
+            ], []);
+
             if ($request->type == EnumMediaTypes::PDF) {
+                $this->validate($request, ['media' => 'required|file|mimes:pdf,doc,jpeg,png']);
             }
 
-            Media::create($request->all());
-
-            return response([
-                'message' => trans('messages.created_success')
-            ], Response::HTTP_OK);
-        } catch (Exception $e) {
-            return response([
-                'message' => $e->getMessage()
-            ], Response::HTTP_BAD_REQUEST);
-        }
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        try {
-            return Media::with(['category'])->findOrFail($id);
-        } catch (Exception $e) {
-            return response([
-                'message' => $e->getMessage()
-            ], Response::HTTP_BAD_REQUEST);
-        }
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        $this->validate($request, [
-            'category_id' => 'nullable|exists:categories,id',
-            'type' => [
-                'required',
-                Rule::in(array_keys(EnumMediaTypes::TYPES_ARRAY))
-            ]
-        ], []);
-
-        if ($request->type == EnumMediaTypes::PDF) {
-            $this->validate($request, ['media' => 'required|file|mimes:pdf,doc,jpeg,png']);
-        }
-
-        if ($request->type == EnumMediaTypes::VIDEO || $request->type == EnumMediaTypes::TEXT) {
-            $this->validate($request, ['media' => 'required|json']);
-        }
-
-        try {
-            ValidateJsonLangKey::validate($request->media);
-
-            if ($request->type == EnumMediaTypes::VIDEO) {
-                $request['media'] = str_replace([
-                    "https://vimeo.com/",
-                    "http://vimeo.com/",
-                    "https://player.vimeo.com/",
-                    "http://player.vimeo.com/",
-                ], '', $request->media);
+            if ($request->type == EnumMediaTypes::VIDEO || $request->type == EnumMediaTypes::TEXT) {
+                $this->validate($request, ['media' => 'required|json']);
             }
+            DB::beginTransaction();
+            try {
+                ValidateJsonLangKey::validate($request->media);
 
-            //TODO: file upload
+                if ($request->type == EnumMediaTypes::VIDEO) {
+
+                    $languages = explode(",", env("LANGUAGES"));
+                    $media = json_decode($request->media, 1);
+
+                    $videosBaseLang = explode(",", $media[$languages[0]]);
+                    $arrayBaseLength = count($videosBaseLang);
+                    $allVideos = [];
+
+                    foreach ($languages as $lang) {
+                        // validate array length for all languages
+                        if (count(explode(",", $media[$lang])) !== $arrayBaseLength) {
+                            throw new Exception("A mesma quantidade de vídeos deve ser preenchida para todas as linguagens ($lang)");
+                        }
+                        $allVideos[$lang] = explode(",", $media[$lang]);
+                    }
+
+
+                    foreach ($videosBaseLang as $k => $video) {
+                        $item = '{';
+                        foreach ($languages as $lang) {
+                            $item .= '"' . $lang . '":"' . trim($allVideos[$lang][$k]) . '",';
+                        }
+                        $item = substr($item, 0, -1);
+                        $item .= "}";
+
+                        Media::create([
+                            'category_id' => $request->category_id,
+                            'media' => $item,
+                            'type' => EnumMediaTypes::VIDEO,
+                        ]);
+                    }
+                }
+
+                //TODO: file upload
+                if ($request->type == EnumMediaTypes::PDF) {
+                }
+                DB::commit();
+                return response([
+                    'message' => trans('messages.created_success')
+                ], Response::HTTP_OK);
+            } catch (Exception $e) {
+                DB::rollBack();
+                return response([
+                    'message' => $e->getMessage()
+                ], Response::HTTP_BAD_REQUEST);
+            }
+        }
+
+        /**
+         * Display the specified resource.
+         *
+         * @param  int  $id
+         * @return \Illuminate\Http\Response
+         */
+        public function show($id)
+        {
+            try {
+                return Media::with(['category'])->findOrFail($id);
+            } catch (Exception $e) {
+                return response([
+                    'message' => $e->getMessage()
+                ], Response::HTTP_BAD_REQUEST);
+            }
+        }
+
+        /**
+         * Update the specified resource in storage.
+         *
+         * @param  \Illuminate\Http\Request  $request
+         * @param  int  $id
+         * @return \Illuminate\Http\Response
+         */
+        public function update(Request $request, $id)
+        {
+            $this->validate($request, [
+                'category_id' => 'nullable|exists:categories,id',
+                'type' => [
+                    'required',
+                    Rule::in(array_keys(EnumMediaTypes::TYPES_ARRAY))
+                ]
+            ], []);
+
             if ($request->type == EnumMediaTypes::PDF) {
+                $this->validate($request, ['media' => 'required|file|mimes:pdf,doc,jpeg,png']);
             }
 
-            $media = Media::findOrFail($id);
-            $media->update($request->all());
+            if ($request->type == EnumMediaTypes::VIDEO || $request->type == EnumMediaTypes::TEXT) {
+                $this->validate($request, ['media' => 'required|json']);
+            }
 
-            return response([
-                'message' => trans('messages.updated_success'),
-                'medias' => $this->index(new Request),
-            ], Response::HTTP_OK);
-        } catch (Exception $e) {
-            return response([
-                'message' => $e->getMessage()
-            ], Response::HTTP_BAD_REQUEST);
+            try {
+                ValidateJsonLangKey::validate($request->media);
+
+                if ($request->type == EnumMediaTypes::VIDEO) {
+                    $request['media'] = str_replace([
+                        "https://vimeo.com/",
+                        "http://vimeo.com/",
+                        "https://player.vimeo.com/",
+                        "http://player.vimeo.com/",
+                    ], '', $request->media);
+                }
+
+                //TODO: file upload
+                if ($request->type == EnumMediaTypes::PDF) {
+                }
+
+                $media = Media::findOrFail($id);
+                $media->update($request->all());
+
+                return response([
+                    'message' => trans('messages.updated_success'),
+                    'medias' => $this->index(new Request),
+                ], Response::HTTP_OK);
+            } catch (Exception $e) {
+                return response([
+                    'message' => $e->getMessage()
+                ], Response::HTTP_BAD_REQUEST);
+            }
+        }
+
+        /**
+         * Remove the specified resource from storage.
+         *
+         * @param  int  $id
+         * @return \Illuminate\Http\Response
+         */
+        public function destroy($id)
+        {
+            try {
+                $media = Media::findOrFail($id);
+                $media->delete();
+
+                return response([
+                    'message' => trans('messages.deleted_success'),
+                    'medias' => $this->index(new Request),
+                ], Response::HTTP_OK);
+            } catch (Exception $e) {
+                return response([
+                    'message' => $e->getMessage()
+                ], Response::HTTP_BAD_REQUEST);
+            }
+        }
+
+        /**
+         * Restore the specified resource from storage.
+         *
+         * @param  int  $id
+         * @return \Illuminate\Http\Response
+         */
+        public function restore($id)
+        {
+            try {
+
+                $media = Media::withTrashed()->findOrFail($id);
+                $media->restore();
+
+                return response([
+                    'message' => trans('messages.restore_success'),
+                    'medias' => $this->index(new Request),
+                ], Response::HTTP_OK);
+            } catch (Exception $e) {
+                return response([
+                    'message' => $e->getMessage()
+                ], Response::HTTP_BAD_REQUEST);
+            }
+        }
+
+        public function types()
+        {
+            return EnumMediaTypes::TYPES;
         }
     }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        try {
-            $media = Media::findOrFail($id);
-            $media->delete();
-
-            return response([
-                'message' => trans('messages.deleted_success'),
-                'medias' => $this->index(new Request),
-            ], Response::HTTP_OK);
-        } catch (Exception $e) {
-            return response([
-                'message' => $e->getMessage()
-            ], Response::HTTP_BAD_REQUEST);
-        }
-    }
-
-    /**
-     * Restore the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function restore($id)
-    {
-        try {
-
-            $media = Media::withTrashed()->findOrFail($id);
-            $media->restore();
-
-            return response([
-                'message' => trans('messages.restore_success'),
-                'medias' => $this->index(new Request),
-            ], Response::HTTP_OK);
-        } catch (Exception $e) {
-            return response([
-                'message' => $e->getMessage()
-            ], Response::HTTP_BAD_REQUEST);
-        }
-    }
-
-    public function types()
-    {
-        return EnumMediaTypes::TYPES;
-    }
-}
